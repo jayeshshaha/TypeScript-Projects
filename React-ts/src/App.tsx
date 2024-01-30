e7a7cc44e0a1aa9b0b5c0a1950c9ecbf6701e308
Jayesh Shaha <106771749+jayeshshaha@users.noreply.github.com>
2024-01-30 21:30:07 +0530

    useContext done

--- React-ts/src/App.tsx
+++ React-ts/src/App.tsx
@@ -1,5 +1,6 @@
 // import Box from "./components/Box"
 
+
 // const App = () => {
 //   return (
 //     <div><Box heading="hellow" count={12} >{"wow"}</Box></div>
@@ -8,8 +9,7 @@
 
 // export default App
 
-
- // Eg2
+// Eg2
 // import Box from "./components/Box";
 
 // const App = () => {
@@ -21,7 +21,6 @@
 // };
 
 // export default App;
-
 
 // !React Hooks - useState
 // import { useState } from "react";
@@ -38,35 +37,70 @@
 
 // export default App;
 
- // Eg2
-import { FormEvent, useState } from "react";
+// Eg2
+// import { FormEvent, useState } from "react";
 
-interface Person {
-  name:string,
-  age:number,
+// interface Person {
+//   name:string,
+//   age:number,
+// }
+
+// const App = () => {
+//   // const [user, setUser] = useState<Person>({
+//   //   name:'',
+//   //   age:0
+//   // })
+//   const [user, setUser] = useState<Person>()
+//   function submitHandler(e:FormEvent<HTMLFormElement>) {
+//     e.preventDefault()
+//     console.log(user);
+
+//   }
+//   return (
+//     <div>
+//       <form onSubmit={submitHandler} >
+//         <input type="number" value={user?.age || ""} onChange={(e) => setUser((prev) =>({...prev!,age:Number(e.target.value)}))} placeholder="Age" />
+
+//         <input type="text" value={user?.name || ""} onChange={(e) => setUser((prev) =>({...prev!,name:e.target.value}))} placeholder="Name" />
+//         <button type="submit">Submit</button>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+// !React Hooks - useContext
+import { ReactNode, createContext, useState } from "react";
+import Box2 from "./components/Box2";
+
+type ThemeType = "light" | "dark";
+interface ThemeContextType {
+  theme: ThemeType;
+  toggleTheme: () => void;
+}
+
+export const ThemeContext = createContext<ThemeContextType>({
+  theme: "light",
+  toggleTheme: () => {},
+});
+
+const ThemeProvider = ({children}:{children:ReactNode})=>{
+  const [theme, setTheme] = useState<ThemeType>("light");
+  const toggleTheme = ()=>{
+    setTheme((prev)=> prev === "light" ? 'dark' : 'light')
+  }
+  return (
+  <ThemeContext.Provider value={{theme,toggleTheme}}>
+    {children}
+  </ThemeContext.Provider>)
 }
 
 const App = () => {
-  // const [user, setUser] = useState<Person>({
-  //   name:'',
-  //   age:0
-  // })
-  const [user, setUser] = useState<Person>()
-  function submitHandler(e:FormEvent<HTMLFormElement>) {
-    e.preventDefault()
-    console.log(user);
-    
-  }
-  return (
-    <div>
-      <form onSubmit={submitHandler} >
-        <input type="number" value={user?.age || ""} onChange={(e) => setUser((prev) =>({...prev!,age:Number(e.target.value)}))} placeholder="Age" /> 
-
-        <input type="text" value={user?.name || ""} onChange={(e) => setUser((prev) =>({...prev!,name:e.target.value}))} placeholder="Name" /> 
-        <button type="submit">Submit</button>
-      </form>
-    </div>
-  );
+  return(
+    <ThemeProvider>
+      <Box2/>
+    </ThemeProvider>)
 };
 
 export default App;
